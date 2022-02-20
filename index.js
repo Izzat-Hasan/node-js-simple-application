df0715cd547c11b995093695325b853a32ad941c
Izzat-Hasan <izzathasan03@gmail.com>
2022-02-20 12:53:38 -0600

    hello world

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var dt = require('./datetime');
+
+
+const server = http.createServer((request, response) => {
+    // Write the request to the log. 
+    console.log(request);
+
+    // Standard Hello World.
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h3>Hello World!</h3>')
+
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
