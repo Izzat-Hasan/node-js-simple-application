238ffd5d61a88025773fdd12a1a05048d540bf4f
Izzat-Hasan <izzathasan03@gmail.com>
2022-02-20 13:38:36 -0600

    i

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ var dt = require('./datetime');
 
 
 const server = http.createServer((request, response) => {
-    // Write the request to the log. 
     console.log(request);
 
-    // Standard Hello World.
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('<h3>Hello World!</h3>')
 
+
+    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
+
+
+    response.write("req.url="+request.url+"<br><br>");
+
+ 
+    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
+    var q = url.parse(request.url, true).query;
+    var txt = q.year + " " + q.month;
+    response.write("txt="+txt);
+
+
+    response.end('<h3>The End.</h3>');
 });
 
 const port = process.env.PORT || 1337;
