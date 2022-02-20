6dcd991ed52f8822cdadc8cbb3083313e06347ca
Izzat-Hasan <izzathasan03@gmail.com>
2022-02-19 18:05:52 -0600

    fix

--- 03-meadowlark.js
+++ 03-meadowlark.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const expressHandlebars= require('express-handlebars')
+const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
@@ -15,12 +15,23 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => res.render('about'))
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+]
 
-// custom404 page
+app.get('/about', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render('about', { fortune: randomFortune })
+})
+
+// custom 404 page
 app.use((req, res) => {
   res.status(404)
-  res.render('400')
+  res.render('404')
 })
 
 // custom 500 page
